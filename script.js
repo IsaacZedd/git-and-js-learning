72106e0bfa39cc5cb7e5545dc8dfc3c3927a6d8c
YishakZewdineh <yishakzewdineh@gmail.com>
2022-03-14 13:22:02 +0300

    Arrays and methods in JS

--- script.js
+++ script.js
@@ -47,3 +47,37 @@ let statuss = "online";
 console.log(
   statuss == "online" ? console.log("Online") : console.log("Offline")
 );
+
+//Arrays
+let emptyArr = [];
+let lotto = [12, 34, 23, 23];
+let shoppingList = ["Laptop", "Phone", "Radio"];
+let mixedArray = ["hello", 12, false, NaN, null, undefined, 23.25];
+mixedArray[0] = "Isaac";
+console.log(mixedArray[2], " is value of myArr at index 2.");
+shoppingList[0] = shoppingList[1];
+shoppingList[3] = "Television";
+shoppingList[shoppingList.length] = "LastOne";
+console.log(shoppingList);
+
+//Array methods
+lotto.push(34); //adds 34 to last pos of lotto array becomes 5 elements
+console.log(lotto);
+shoppingList.push("Some", "Items"); //added 2 values to the shoppingList array
+console.log(shoppingList);
+shoppingList.pop();
+shoppingList.pop();
+shoppingList.pop();
+console.log(shoppingList);
+
+mixedArray.unshift("Developer");
+mixedArray.unshift("The", "Most Influential");
+console.log(mixedArray);
+mixedArray.shift();
+mixedArray.shift();
+console.log(mixedArray);
+
+let concatedArray = emptyArr.concat(lotto);
+console.log(concatedArray);
+console.log(shoppingList.includes("Television"));
+console.log(shoppingList.includes("Television", 3)); //starting from specified index = 3
